d1fae625c2ca8a01e0d202d1568355c97c01c5af
vishal gupta <vg.vishal.gupta.10@gmail.com>
2020-03-12 11:38:16 +0530

    added

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+// ########## Import Dependencies Here ##########
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// ########## Import Screens Here ##########
+
+// ########## Import Components Here ##########
+import App from './js/components/app';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#app')
+)
